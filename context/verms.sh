6621b7c281d1e7aa0b38c9c5ab7e248c3ac0a631
Aman Verma <amanraoverma@gmail.com>
2025-01-15 15:04:19 -0600

    Use function for dnf.

--- context/verms.sh
+++ context/verms.sh
@@ -9,13 +9,18 @@ ln --no-target-directory -s ../run /var/run
 # Dockerfile COPY.
 cp --no-target-directory -vR context/etc /etc
 
+# Don't prompt
+autodnf() {
+    command dnf --assumeyes "$@"
+}
+
 # Fix package reasons
-dnf -y mark dependency '*' >/dev/null
-dnf rq --leaves | xargs -d '\n' dnf -y mark user >/dev/null
-dnf -y mark user fwupd rpm-ostree qemu-user-static-aarch64
+autodnf mark dependency '*' >/dev/null
+autodnf mark user $(dnf repoquery --leaves) >/dev/null
+autodnf mark user fwupd rpm-ostree qemu-user-static-aarch64
 
 # Removals
-dnf -y remove \
+autodnf remove \
     bash-color-prompt \
     bash-completion \
     bind-utils \
@@ -30,28 +35,28 @@ sed -i '/^enabled=0/{s/0/1/}' /etc/yum.repos.d/google-chrome.repo
 sed -i '/^enabled=1/{s/1/0/}' /etc/yum.repos.d/fedora-cisco-openh264.repo
 
 # RPM Fusion
-dnf -y install \
+autodnf install \
     "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$1.noarch.rpm" \
     "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$1.noarch.rpm"
 
 # Codecs
-dnf -y swap mesa-va-drivers mesa-va-drivers-freeworld
-dnf -y swap '(ffmpeg-free or libswscale-free or libavformat-free or libavfilter-free or libavutil-free or libavcodec-free)' ffmpeg-libs
+autodnf swap mesa-va-drivers mesa-va-drivers-freeworld
+autodnf swap '(ffmpeg-free or libswscale-free or libavformat-free or libavfilter-free or libavutil-free or libavcodec-free)' ffmpeg-libs
 
 # Mitigate https://bugzilla.redhat.com/show_bug.cgi?id=2332429
-dnf -y swap OpenCL-ICD-Loader ocl-icd
+autodnf swap OpenCL-ICD-Loader ocl-icd
 
 # Host packages
 grep -Ev '^#|^$' context/host.txt |
-    xargs -d '\n' dnf -y install
+    xargs -d '\n' autodnf install
 grep -Ev '^#|^$' context/host-no-weak-deps.txt |
-    xargs -d '\n' dnf -y --setopt install_weak_deps=False install
+    xargs -d '\n' autodnf --setopt install_weak_deps=False install
 
 # Install google-chrome-stable. Taken from
 # https://github.com/travier/fedora-sysexts/blob/047ab6b890/google-chrome/Containerfile
 mv /opt /opt.bk
 mkdir /opt
-dnf -y install google-chrome-stable
+autodnf install google-chrome-stable
 mv /opt/google/chrome /usr/lib/google-chrome
 ln -sf /usr/lib/google-chrome/google-chrome /usr/bin/google-chrome-stable
 for i in 16 24 32 48 64 128 256; do
