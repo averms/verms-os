19260a201226db36ac2b82bd7acbec1c40a066b8
Aman Verma <amanraoverma@gmail.com>
2025-03-23 20:23:15 -0500

    Remove dnf5 caching.
    It's not necessary anymore since there isn't much churn. I don't need to
    test changes on my home connection too often.

--- build_scripts/full-verms.sh
+++ build_scripts/full-verms.sh
@@ -55,6 +55,5 @@ systemctl disable nvidia-powerd.service
 
 # If it tries to autoremove, something went wrong.
 dnf --assumeno autoremove
-# Clean var except for var/lib/cache/libdnf5 which is mount type=cache.
-find /var -mindepth 2 -maxdepth 2 -not -name libdnf5 -print0 | xargs -0 rm -r
+rm -r /var/*
 bootc container lint
