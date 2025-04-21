4e640591ec03284fd74d52d38c31b47ba68490ef
Aman Verma <amanraoverma@gmail.com>
2025-04-20 20:31:08 -0500

    build_scripts: disable locate database creation.
    I don't use locate(1) and I don't think anything else on the system does either.

--- build_scripts/final.sh
+++ build_scripts/final.sh
@@ -53,6 +53,7 @@ systemctl enable tailscaled.service
 systemctl enable bootc-fetch-apply-updates.timer
 systemctl disable avahi-daemon.service
 systemctl disable nvidia-powerd.service
+systemctl disable plocate-updatedb.timer
 
 # If it tries to autoremove, something went wrong.
 dnf --assumeno autoremove
