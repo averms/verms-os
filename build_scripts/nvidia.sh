73d826d6d34389eb12ab0dc6a725847ebcdd93ac
Aman Verma <amanraoverma@gmail.com>
2025-08-30 22:24:42 -0400

    force building proprietary nvidia driver

--- build_scripts/nvidia.sh
+++ build_scripts/nvidia.sh
@@ -3,5 +3,8 @@ set -eu
 
 . build_scripts/_lib.sh
 
+# force proprietary drivers because i have a GTX 10 series GPU
+echo "%_without_kmod_nvidia_detect 1" >/etc/rpm/macros.nvidia-kmod
+
 autodnf install akmod-nvidia "kernel-devel-matched-${kernel_ver}"
 akmods --force --kernels "${kernel_ver}"
