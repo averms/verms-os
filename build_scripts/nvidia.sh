c026e609b3aa5c705c1908185d675b9868309304
Aman Verma <amanraoverma@gmail.com>
2025-08-30 21:14:06 -0400

    pin nvidia driver version to try to fix GTX 1070 issue

--- build_scripts/nvidia.sh
+++ build_scripts/nvidia.sh
@@ -3,5 +3,5 @@ set -eu
 
 . build_scripts/_lib.sh
 
-autodnf install akmod-nvidia "kernel-devel-matched-${kernel_ver}"
+autodnf install akmod-nvidia-3:560.35.03-1.fc41.x86_64 "kernel-devel-matched-${kernel_ver}"
 akmods --force --kernels "${kernel_ver}"
