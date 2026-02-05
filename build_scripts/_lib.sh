7b91a08b58b85b0bfe7879825ab9960b5182be8d
Aman Verma <amanraoverma@gmail.com>
2026-02-05 12:59:38 -0500

    quiet dnf

--- build_scripts/_lib.sh
+++ build_scripts/_lib.sh
@@ -1,7 +1,7 @@
 # --best causes dnf to fail if it can't install the latest version of everything. This
 # prevents downgrading if rpmfusion is not up to date.
 autodnf() {
-    dnf --assumeyes --best "$@"
+    dnf --quiet --assumeyes --best "$@"
 }
 
 from_file() {
