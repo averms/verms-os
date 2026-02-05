fed3da044f82adcf318f12e313c81473373c6364
Aman Verma <amanraoverma@gmail.com>
2026-02-05 13:10:26 -0500

    make dnf not truncate installation progress to 80 chars
    This makes logs more readable. The problem is described in more detail
    on Chris Siebenmann's blog[^1].
    
    [^1]: https://utcc.utoronto.ca/~cks/space/blog/linux/FedoraDnf5SmartOutputCurse

--- build_scripts/_lib.sh
+++ build_scripts/_lib.sh
@@ -1,7 +1,7 @@
 # --best causes dnf to fail if it can't install the latest version of everything. This
 # prevents downgrading if rpmfusion is not up to date.
 autodnf() {
-    dnf --quiet --assumeyes --best "$@"
+    FORCE_COLUMNS=120 dnf --quiet --assumeyes --best "$@"
 }
 
 from_file() {
