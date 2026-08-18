aaca9c764f1221a7a61a60e55e2e685d7d51e27e
Aman Verma <code@averms.org>
2026-08-18 03:11:15 -0400

    cleanups
    1. Remove discord because I wasn't using it.
    2. Remove the exclusions because they were fixed.
    3. Remove sushi because the Flathub flatpak works now that I'm on Fedora 44.

--- build_scripts/final.sh
+++ build_scripts/final.sh
@@ -44,12 +44,7 @@ systemctl enable bootc-fetch-apply-updates.timer
 systemctl enable tailscaled.service
 
 # If it tries to autoremove, something went wrong.
-# The exclusions come from steam installing libnsl.i686 and libXScrnSaver.i686. DNF5
-# seems to want x86_64 versions of every package installed as a dependency. Rather than
-# fight it, we'll just ignore it.
-dnf autoremove --assumeno \
-    --exclude libnsl.x86_64 \
-    --exclude libXScrnSaver.x86_64
+dnf autoremove --assumeno
 
 dnf clean all
 rm -r /var/*
