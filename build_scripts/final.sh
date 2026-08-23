5c4c430e78ce68495e0f9804865dacad48abc0b8
Aman Verma <code@averms.org>
2026-08-22 21:50:44 -0400

    try LTS kernel again lol

--- build_scripts/final.sh
+++ build_scripts/final.sh
@@ -31,6 +31,11 @@ autodnf "do" --action=remove \
 rm /opt
 mkdir /opt
 
+# Use LTS kernel
+autodnf copr enable kwizart/kernel-longterm-6.18
+autodnf install kernel-longterm kernel-longterm-modules-extra
+autodnf remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+
 # Host packages
 autodnf install $(from_file build_scripts/packages.txt)
 autodnf --setopt install_weak_deps=False install $(from_file build_scripts/packages_no_weak_deps.txt)
