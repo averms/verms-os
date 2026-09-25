e960e4abe26971d5aa53f9ec49195005a9a47438
Aman Verma <code@averms.org>
2026-09-25 15:28:18 -0400

    double-check that nvidia kmod was built

--- build_scripts/nvidia.sh
+++ build_scripts/nvidia.sh
@@ -8,3 +8,9 @@ echo "%_without_kmod_nvidia_detect 1" >/etc/rpm/macros.nvidia-kmod
 
 autodnf install akmod-nvidia "kernel-devel-matched-${kernel_ver}"
 akmods --force --kernels "${kernel_ver}"
+
+# shellcheck disable=SC2144
+if [ ! -f /var/cache/akmods/nvidia/kmod-nvidia-"${kernel_ver}"*.rpm ]; then
+    echo "akmods failed to build kmod-nvidia" >&2
+    exit 1
+fi
